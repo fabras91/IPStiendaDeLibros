1480e6cede735f7bf54d926aa3ce71543b22d8f6
RobertQH123 <robertqhjaki@gmail.com>
2022-08-05 09:25:59 -0500

    acomodando carpetas

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <html>
 <meta http-equiv="Content-Type"'.' content="text/html; charset=utf8"/>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style/list.css">
+<link rel="stylesheet" href="style/style.css">
 <body>
 <?php
 session_start();
